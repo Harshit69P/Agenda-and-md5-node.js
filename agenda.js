a1821f6b88780bd9464c5936a2db22325ba86f47
Harshit Gupta <104277488+Harshit69P@users.noreply.github.com>
2023-12-26 14:52:12 +0530

    Add files via upload

---
+++ agenda.js
@@ -0,0 +1,28 @@
+// const Agenda = require('agenda');
+
+// const agenda = new Agenda({db: {address: 'mongodb://localhost/agenda'}});
+
+// // Define job
+// agenda.define('printPrakriti', (job, done) => {
+//   console.log('Hello, kittu');
+//   done();
+// });
+// console.log('harshit');
+// // Schedule job
+// agenda.on('ready', () => {
+//   agenda.every('1 minute', 'printPrakriti');
+//   agenda.start();
+// });
+
+// const crypto = require('crypto');
+
+// function generateMD5Hash(input) {
+//     return crypto.createHash('md5').update(input).digest('hex'); //
+// }
+
+// console.log(generateMD5Hash('Hello, my name is harshit gupta'));
+
+// direct md5 usage
+// var md5 = require('md5');
+
+// console.log(md5('Hello, World!'));
